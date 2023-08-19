c1457a8f4e3fe39a272a958c38eeaaad2358a802
Keutmann <keutmann@yahoo.com>
2023-08-19 14:19:34 +0200

    MyAvatar fix

--- src/js/components/user/Avatar.tsx
+++ src/js/components/user/Avatar.tsx
@@ -5,9 +5,8 @@ import Show from '../helpers/Show';
 import SafeImg from '../SafeImg';
 
 import profileManager from '../../dwotr/ProfileManager';
-import { ProfileMemory } from '../../dwotr/model/ProfileRecord';
 import { useKey } from '@/dwotr/hooks/useKey';
-import { useIsMounted } from '@/dwotr/hooks/useIsMounted';
+import { useProfile } from '@/dwotr/hooks/useProfile';
 
 type Props = {
   str: string | undefined;
@@ -18,46 +17,30 @@ type Props = {
   width: number;
 };
 
-function getProfile(hexKey: string, profile: any, hasError: boolean, hidePicture: boolean = false, width?: number) {
-  let hasPic = profile?.picture && !hasError && !hidePicture && !SocialNetwork.isBlocked(hexKey);
-  let avatar = !hasPic ? profileManager.createImageUrl(hexKey, width) : '';
+// Create a profile state object from the profile data
+function getProfileState(profile: any, hasError: boolean, props: Props) {
+  let hexKey = profile.key;
+  let hasPic =
+    profile?.picture && !hasError && !props?.hidePicture && !SocialNetwork.isBlocked(hexKey);
+  let avatar = !hasPic ? profileManager.createImageUrl(hexKey, props.width) : '';
+  let isActive = ['online', 'active'].includes(profile.activity || '');
 
-  return { ...profile, hasPic, avatar };
+  return { ...profile, hasPic, avatar, isActive, activity: props?.activity };
 }
 
+// MyAvatar - A component to display an avatar
 const MyAvatar: React.FC<Props> = (props) => {
-  const isMounted = useIsMounted();
-  const { hexKey, uid } = useKey(props.str);
-
-  const [profile, setProfile] = useState<any>(
-    getProfile(hexKey, profileManager.getMemoryProfile(uid), false, props?.hidePicture, props.width),
-  );
-
-  const [activity] = useState<string | null>(null); // TODO
-
+  const { hexKey } = useKey(props.str);
   const [hasError, setHasError] = useState<boolean>(false);
+  const profile = useProfile(hexKey);
+  const [state, setState] = useState<any>(getProfileState(profile, hasError, props));
 
   useEffect(() => {
-    const handleEvent = (e: any) => {
-      if (!isMounted()) return;
-      let p = e.detail as ProfileMemory;
-
-      setProfile(getProfile(hexKey, p, hasError, props?.hidePicture, props.width));
-    };
-
-    // uid follows the hexKey automatically
-    setProfile(getProfile(hexKey, profileManager.getMemoryProfile(uid), false, props?.hidePicture, props.width));
-
-    let unsub = profileManager.subscribe(hexKey, handleEvent);
-    return () => {
-      unsub?.();
-    };
-  }, [hexKey]);
+    setState(getProfileState(profile, hasError, props));
+  }, [profile, hasError, props?.activity]);
 
   const width = props.width;
-  const isActive = ['online', 'active'].includes(activity || '');
-  const hasPic = profile?.hasPic;
-  //const hasPic = picture && !hasError && !props.hidePicture && !SocialNetwork.isBlocked(hexKey || '');
+  const hasPic = state.hasPic;
 
   return (
     <div
@@ -68,28 +51,30 @@ const MyAvatar: React.FC<Props> = (props) => {
       }}
       className={`inline-flex flex-col flex-shrink-0 items-center justify-center relative select-none ${
         hasPic ? 'has-picture' : ''
-      } ${props.showTooltip ? 'tooltip' : ''} ${isActive ? activity : ''}`}
+      } ${props.showTooltip ? 'tooltip' : ''} ${state.isActive ? state.activity : ''}`}
       onClick={props.onClick}
     >
       <div>
         <Show when={hasPic}>
           <SafeImg
             className="object-cover rounded-full"
-            src={profile?.picture || ''}
+            src={state.picture || ''}
             width={width}
             square={true}
             onError={() => setHasError(true)}
           />
         </Show>
         <Show when={!hasPic}>
-          <img width={width} className="max-w-full rounded-full" src={profile?.avatar || ''} />
+          <img width={width} className="max-w-full rounded-full" src={state.avatar || ''} />
         </Show>
       </div>
-      <Show when={props.showTooltip && profile?.name}>
-        <span className="tooltiptext">{profile?.name}</span>
+      <Show when={props.showTooltip && state.name}>
+        <span className="tooltiptext">{state.name}</span>
       </Show>
     </div>
   );
 };
 
+
 export default MyAvatar;
+
--- src/js/dwotr/hooks/useProfile.ts
+++ src/js/dwotr/hooks/useProfile.ts
@@ -1,21 +1,18 @@
 import { useEffect, useState } from 'react';
 import profileManager from '../ProfileManager';
 import ProfileRecord, { ProfileMemory } from '../model/ProfileRecord';
-import { ProfileEvent } from '../network/ProfileEvent';
 import { ID } from '@/utils/UniqueIds';
+import { useIsMounted } from './useIsMounted';
 
 
 // Address can be of type hex of BECH32
 export const useProfile = (address: string) => {
 
-  //console.log('dwotr.useProfile.address', address)
+  const isMounted = useIsMounted();
 
   const [profile, setProfile] = useState<ProfileRecord>(() => profileManager.getMemoryProfile(ID(address)));
 
   useEffect(() => {
-    //if (!address) return;
-    //console.log('dwotr.useProfile.mount', address);
-
     let id = ID(address);
 
     let mem = profileManager.getMemoryProfile(id);
@@ -24,25 +21,20 @@ export const useProfile = (address: string) => {
     }
 
     const handleEvent = (e: any) => {
+      if(!isMounted()) return; // ignore events after unmount
       let p = e.detail as ProfileMemory;
       if(!p || p.id != id) return; // not for me
 
       setProfile(prevProfile => {
         if (p.created_at <= prevProfile.created_at) return prevProfile; // ignore older events
-        return p;
+        return { ...p }; // Make sure to copy the object, otherwise React may not re-render
       });
     };
 
-    ProfileEvent.add(handleEvent);
-
-    let unsub = profileManager.subscribe(address);
-
-    return () => {
-      //console.log('dwotr.useProfile.unmount', address);
-      ProfileEvent.remove(handleEvent);
-      unsub?.();
-    };
+    return profileManager.subscribe(address, handleEvent);
   }, [address]);
 
   return profile;
 };
+
+
