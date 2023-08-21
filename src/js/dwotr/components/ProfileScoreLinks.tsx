7172e19333d7e9ba1556147400557219fa680347
Keutmann <keutmann@yahoo.com>
2023-08-21 10:50:26 +0200

    Small refact

--- src/js/dwotr/components/ProfileScoreLinks.tsx
+++ src/js/dwotr/components/ProfileScoreLinks.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'preact/hooks';
 import TrustScore from '../model/TrustScore';
 import useVerticeMonitor from '../hooks/useVerticeMonitor';
 import { RenderScoreDistrustLink, RenderScoreTrustLink } from './RenderGraph';
-import { ID } from '@/utils/UniqueIds';
 import { useKey } from '../hooks/useKey';
 
 const ProfileScoreLinks = ({ str }: any) => {
-  const { bech32Key } = useKey(str);
+  const { bech32Key, uid } = useKey(str);
   const [score, setScore] = useState<TrustScore | undefined>(undefined);
 
-  const wot = useVerticeMonitor(ID(str), undefined, '') as any;
+  const wot = useVerticeMonitor(uid, undefined, '') as any;
 
   useEffect(() => {
     setScore(() => wot.vertice?.score as TrustScore);
